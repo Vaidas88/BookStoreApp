4d5c70780abeb364a54853176011a4fa0774967b
Vaidas88 <v.butkus@outlook.com>
2021-11-26 19:43:57 +0200

    init

---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreApp.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+
+        public Book(string title, string description, int amount)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Amount = amount;
+        }
+
+        public void PrintBook()
+        {
+            Console.WriteLine($"title: {this.Title}, description: {this.Description}, amount: {this.Amount}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BookStoreApp.Models;
+
+var books = new List<Book>();
+
+while (true)
+{
+	Console.WriteLine("Please enter command: 'Add', 'List', 'Delete {title}', Exit: ");
+	var command = Console.ReadLine();
+
+	if (command.ToLower() == "exit")
+	{
+		return;
+	}
+	else if (command.ToLower() == "add")
+	{
+		Console.WriteLine("Please enter the Title");
+		var title = Console.ReadLine();
+
+		Console.WriteLine("Please enter the Description");
+		var description = Console.ReadLine();
+
+		Console.WriteLine("Please enter the Amount");
+		var amount = Convert.ToInt32(Console.ReadLine());
+
+		var Book = new Book(title, description, amount);
+		books.Add(Book);
+	}
+	else if (command.ToLower() == "list")
+	{
+		foreach (var book in books)
+		{
+			book.PrintBook();
+		}
+	}
+
+	
+}
