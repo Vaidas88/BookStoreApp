3a1d6cbb36523b4beb7e552ce8374f91566d572c
Vaidas88 <v.butkus@outlook.com>
2021-11-26 19:58:33 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,9 +20,29 @@ while (true)
 		var description = Console.ReadLine();
 
 		Console.WriteLine("Please enter the Amount");
-		var amount = Convert.ToInt32(Console.ReadLine());
+		var amount = 0;
+        try
+        {
+			amount = Convert.ToInt32(Console.ReadLine());
+		}
+		catch (Exception ex)
+        {
+			Console.WriteLine("Wrong amount");
+        }
 
 		var Book = new Book(title, description, amount);
+
+		if(books.Count > 0)
+        {
+			foreach (var book in books)
+            {
+				if(book.Title == title)
+                {
+					Console.WriteLine("Duplicate entry, try again.");
+					continue;
+                }
+            }
+        }
 		books.Add(Book);
 	}
 	else if (command.ToLower() == "list")
@@ -32,6 +52,16 @@ while (true)
 			book.PrintBook();
 		}
 	}
-
-	
+	else if (command.ToLower().Contains("delete"))
+    {
+		var titleToDelete = command.Substring(command.IndexOf(' ') + 1);
+		for(int i = 0; i < books.Count; i++)
+        {
+			if(books[i].Title == titleToDelete)
+            {
+				books.RemoveAt(i);
+				continue;
+            }
+        }
+    }
 }
